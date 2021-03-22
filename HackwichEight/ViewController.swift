9cbd2cf2bdf44241275d43a7ba75d8ef9b9af2a6
esamson6 <77295762+esamson6@users.noreply.github.com>
2021-03-22 06:17:38 -1000

    Hackwich Eight Part 3

--- HackwichEight/ViewController.swift
+++ HackwichEight/ViewController.swift
@@ -9,26 +9,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var targetLabel: UILabel!
     @IBOutlet weak var slider: UISlider!
     
-    var currentValue = 0
+    var currentValue = 50
+    var targetValue = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        slider.value = 0
+        //currentValue = Int(slider.value)
+        //targetValue = Int.random(in: 0...100)
     }
-    
     
     @IBAction func sliderHasMoved(_ sender: Any) {
         //let's print the value of the slider
         print("The value of the slider is:\(slider.value)")
         currentValue = Int(slider.value)
+        //int has an instance method called random
+        //targetValue = Int.random(in: 0...100)
+    
+    }
+    //function to start a new round so that we get a new target value
+    
+    func startNewRound(){
+        targetValue = Int.random(in: 0...100)
+        currentValue = Int(slider.value)
         
     }
     
+    func updateTargetLabel(){
+        targetLabel.text = String(targetValue)
+        startNewRound()
+    }
     @IBAction func myGuessButtonPressed(_ sender: Any) {
         
-        let message = "The value is \(currentValue)"
+        let message = "The value is \(currentValue)" + "\n The Target Value is: \(targetValue)"
+        
     //1. create alert view
         let alert = UIAlertController(title:"Hello World", message: message, preferredStyle: .alert)
     //2. button that user taps to dismiss view controller
@@ -37,7 +55,7 @@ class ViewController: UIViewController {
         alert.addAction(action)
       //4. present alertview on the screen
         present(alert,animated: true, completion: nil)
-    
+       startNewRound()
     
     
     
