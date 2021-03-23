94b50b8a253cba3df9391e4d9f07946b94a2ee21
esamson6 <77295762+esamson6@users.noreply.github.com>
2021-03-23 05:49:49 -1000

    Completed Part 3 and added comments

--- HackwichEight/ViewController.swift
+++ HackwichEight/ViewController.swift
@@ -8,10 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //create a target label for the number to randomly change for the guessing game
     @IBOutlet weak var targetLabel: UILabel!
+    //when the slider is moved the player is guessing as close or exactly the same randomly changed number.
     @IBOutlet weak var slider: UISlider!
     
+    //on the slider we needed to give value to the movement of the slider
     var currentValue = 50
     var targetValue = 0
     
@@ -33,11 +35,21 @@ class ViewController: UIViewController {
         //int has an instance method called random
         //targetValue = Int.random(in: 0...100)
     }
+    
+         //function to start a new round so that we get a new target value
+         //When the new round button is pressed the random number should change for the new game
+         func startNewRound() {
+        //to make the target number change into random numbers
+         let randomNumber = Int.random(in: 0...100)
+        //this target number or random number should be close to my guess or exactly same to my guess
+        targetLabel.text = String(randomNumber)
+    
+    }
+    
 
     @IBAction func myGuessButtonPressed(_ sender: Any) {
-        
+      //your guess will be on the screen with the number that you have selected with the use of moving the slider.
         let message = "Your guess is \(currentValue)" + "\n The Target Value for this round was: \(targetValue)"
-        
     //1. create alert view
         let alert = UIAlertController(title:"Guess the Number Game", message: message, preferredStyle: .alert)
     //2. button that user taps to dismiss view controller
@@ -46,15 +58,9 @@ class ViewController: UIViewController {
         alert.addAction(action)
       //4. present alertview on the screen
         present(alert,animated: true, completion: nil)
+     //a new game will start when the new round is pressed
        startNewRound()
-    }
-    //function to start a new round so that we get a new target value
-    
-    func startNewRound() {
-        let randomNumber = Int.random(in: 0...100)
-        targetLabel.text = String(randomNumber)
-    
-    
+
     
     }
     
