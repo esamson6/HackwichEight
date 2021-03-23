ccbe41df034c89c7be9a5357404158bc5205b994
esamson6 <77295762+esamson6@users.noreply.github.com>
2021-03-23 05:29:19 -1000

    Completed Part 3

--- HackwichEight/ViewController.swift
+++ HackwichEight/ViewController.swift
@@ -15,12 +15,15 @@ class ViewController: UIViewController {
     var currentValue = 50
     var targetValue = 0
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        slider.value = 0
+        slider.value = 50
         //currentValue = Int(slider.value)
         //targetValue = Int.random(in: 0...100)
+        //call startNewRound
+        startNewRound()
     }
     
     @IBAction func sliderHasMoved(_ sender: Any) {
@@ -29,33 +32,27 @@ class ViewController: UIViewController {
         currentValue = Int(slider.value)
         //int has an instance method called random
         //targetValue = Int.random(in: 0...100)
-    
     }
-    //function to start a new round so that we get a new target value
-    
-    func startNewRound(){
-        targetValue = Int.random(in: 0...100)
-        currentValue = Int(slider.value)
-        
-    }
-    
-    func updateTargetLabel(){
-        targetLabel.text = String(targetValue)
-        startNewRound()
-    }
+
     @IBAction func myGuessButtonPressed(_ sender: Any) {
         
-        let message = "The value is \(currentValue)" + "\n The Target Value is: \(targetValue)"
+        let message = "Your guess is \(currentValue)" + "\n The Target Value for this round was: \(targetValue)"
         
     //1. create alert view
-        let alert = UIAlertController(title:"Hello World", message: message, preferredStyle: .alert)
+        let alert = UIAlertController(title:"Guess the Number Game", message: message, preferredStyle: .alert)
     //2. button that user taps to dismiss view controller
-        let action = UIAlertAction(title: "Awesome", style:.default, handler: nil)
+        let action = UIAlertAction(title: "New Round", style:.default, handler: nil)
      //3. add the button to the alertview
         alert.addAction(action)
       //4. present alertview on the screen
         present(alert,animated: true, completion: nil)
        startNewRound()
+    }
+    //function to start a new round so that we get a new target value
+    
+    func startNewRound() {
+        let randomNumber = Int.random(in: 0...100)
+        targetLabel.text = String(randomNumber)
     
     
     
